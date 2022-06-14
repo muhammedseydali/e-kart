f3e4b7abdc721bb9712920b8419ebdb4bb423428
muhammed seydali <60098100+muhammedseydali@users.noreply.github.com>
2022-06-14 16:38:48 +0530

    search functionality error cleared now working properly

--- store/views.py
+++ store/views.py
@@ -59,7 +59,7 @@ def search(request):
     if 'keyword' in request.GET:
         keyword = request.GET['keyword']
         if keyword:
-            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) 
+            products = Product.objects.order_by('-crated_date').filter(Q(description__icontains=keyword) 
             | Q(product_name__icontains=keyword))
             product_count = products.count()
         context = {
